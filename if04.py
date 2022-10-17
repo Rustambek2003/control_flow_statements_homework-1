7e341fcac7b06a5b44d32ed5e1dbac657bed6103
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-17 21:22:41 +0500

    if_04

--- if04.py
+++ if04.py
@@ -8,4 +8,12 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    m = 0
+    if a > 0 and b > 0 and c > 0:
+        m += 3
+    elif a > 0 and b > 0 or b > 0 and c > 0 or a > 0 and c > 0:
+        m += 2
+    else:
+        m += 1
+    return m
+print(main(5,-9,-6))
