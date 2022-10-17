91190391e20e632a47b9cbc7e532ebd3919707fe
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-17 21:40:26 +0500

    if_06

--- if06.py
+++ if06.py
@@ -13,4 +13,27 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    m = 0
+    n = 0
+    if a > 0:
+        m += 1
+    else:
+        n += 1
+
+    if b > 0:
+        m += 1
+    else:
+        n += 1
+
+    if c > 0:
+        m += 1
+    else:
+        n += 1
+    s = 0
+    if m > n:
+        s = "there are a lot of positive numbers"
+    else:
+        s = "there are a lot of negative numbers"
+
+    return s
+print(main(9,-5,6))
